9dc13fe68a5e89cd6fc33cd53a1ca08d8c43c159
unknown <lovemychobits@sohu.com>
2016-05-06 14:31:21 +0800

    新增客户端网络相关

---
+++ client-net-script/script/net/MessageHeader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageHeader
+{
+    [System.Serializable]
+    [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential, Pack = 4)]
+    public struct Header
+    {
+        public int messageSize;
+        public int messageCommand;
+        public uint messageCheck;
+
+        public Header(int message_size, int message_command)
+        {
+            messageSize = message_size;
+            messageCommand = message_command;
+            messageCheck = 0xA1B2C3D4;
+        }
+
+        public override string ToString()
+        {
+            string retValue = string.Format("messageSize = {0}, messageCommand = {1}, messageCheck = {2}", messageSize, messageCommand, messageCheck);
+            return retValue;
+        }
+    };
+}
---
+++ client-net-script/script/net/ObjectBytesTrans.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBytesTrans
+{
+    public static object BytesToStruct(byte[] bytes, System.Type obj_type)
+    {
+        int size = System.Runtime.InteropServices.Marshal.SizeOf(obj_type);
+        System.IntPtr buffer = System.Runtime.InteropServices.Marshal.AllocHGlobal(size);
+        try
+        {
+            System.Runtime.InteropServices.Marshal.Copy(bytes, 0, buffer, size);
+            return System.Runtime.InteropServices.Marshal.PtrToStructure(buffer, obj_type);
+        }
+        finally
+        {
+            System.Runtime.InteropServices.Marshal.FreeHGlobal(buffer);
+        }
+    }
+
+    public static byte[] StructToBytes(object obj)
+    {
+        int size = System.Runtime.InteropServices.Marshal.SizeOf(obj);
+        System.IntPtr buffer = System.Runtime.InteropServices.Marshal.AllocHGlobal(size);
+        try
+        {
+            System.Runtime.InteropServices.Marshal.StructureToPtr(obj, buffer, false);
+            byte[] bytes = new byte[size];
+            System.Runtime.InteropServices.Marshal.Copy(buffer, bytes, 0, size);
+            return bytes;
+        }
+        finally
+        {
+            System.Runtime.InteropServices.Marshal.FreeHGlobal(buffer);
+        }
+    }
+}
